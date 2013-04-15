008d1bf9d33cf46c5694455f5037ae021bb666c2
Mark T. Holder <mtholder@gmail.com>
2013-04-15 00:01:52 -0500

    newer tests added to run.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 python test_tnrs.py 'Pan trolodytes'
 python test_gols_tree_ids.py
+python test_gols_get_source_newick.py
+python test_taxo_argus_source.py
 cat data/doi_list.txt | python test_phylografter_doi.py
 cat data/bad/doi_list.txt | python test_phylografter_doi.py 0
