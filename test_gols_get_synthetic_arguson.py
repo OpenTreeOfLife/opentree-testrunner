ca6d2dc2bcccf81de6235140a1bf2a00ac817b45
Mark T. Holder <mtholder@gmail.com>
2013-04-27 00:38:43 -0500

    simple test of getting the synthetic tree arguson

---
+++ test_gols_get_synthetic_arguson.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+import requests
+import json
+from opentreetesting import config, summarize_json_response
+DOMAIN = config('host', 'golshost')
+SUBMIT_URI = DOMAIN + '/db/data/ext/GoLS/graphdb/getSyntheticTree'
+payload = {
+    'treeID' : 'otol.draft.22',
+    'format' : 'arguson', 
+    'maxDepth' : 4
+}
+if len(sys.argv) > 1:
+    payload['treeID'] = sys.argv[1:]
+headers = {
+    'content-type' : 'application/json',
+    'accept' : 'application/json',
+}
+resp = requests.post(SUBMIT_URI,
+                     headers=headers,
+                     data=json.dumps(payload),
+                     allow_redirects=True)
+summarize_json_response(resp) or sys.exit(1)
