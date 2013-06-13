564599b3c4df7bc347882d1188efc23b31929f23
Mark T. Holder <mtholder@gmail.com>
2013-06-13 15:02:43 -0500

    removing more cruft output

--- opentreetesting.py
+++ opentreetesting.py
@@ -37,9 +37,7 @@ def summarize_json_response(resp):
         print 'Non json resp is:', resp.text
         return False
     if isinstance(results, unicode) or isinstance(results, str):
-        print "repr(res.json)=>  %s" % repr(results)
         er = json.loads(results)
-        print type(er)
         print json.dumps(er, sort_keys=True, indent=4)
         sys.stderr.write('Getting JavaScript string. Object expected.\n')
         return False
