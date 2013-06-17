35c6ef3b9905a33ac44841414464ba475cd3c627
Mark T. Holder <mtholder@gmail.com>
2013-06-17 15:54:54 -0500

    added test_gols_get_synthetic_arguson.py  to treemachine tests

--- run_treemachine_tests.sh
+++ run_treemachine_tests.sh
@@ -4,3 +4,4 @@ python test_gols_tree_ids.py
 python test_gols_get_id_of_draft.py
 python test_gols_get_source_newick.py
 python test_gols_get_source_arguson.py
+python test_gols_get_synthetic_arguson.py
