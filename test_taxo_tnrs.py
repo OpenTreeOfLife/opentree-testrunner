5981376e3ad2035f12a6a2761e62488e8f5f9ba8
Mark T. Holder <mtholder@gmail.com>
2014-11-18 00:15:27 +0100

    fuzzy and no fuzzy tests

--- test_taxo_tnrs.py
+++ test_taxo_tnrs.py
@@ -2,11 +2,23 @@
 import sys
 from peyotl.utility import get_test_config
 from peyotl.api import APIWrapper
-def test_v2_tnrs():
+names = ['Pan trodlogytes', 'Homo sapphire', 'Plantago', 'Morpho peleides', 'Eleocharis',]
+
+def test_v2_tnrs_no_fuzzy():
+    cfg = get_test_config()
+    taxo = APIWrapper(config=cfg).taxomachine
+    resp = taxo.TNRS(names, context_name="All life", fuzzy_matching=False)
+    print resp
+    assert len(resp['matched_name_ids']) == 3
+    assert 'Homo sapphire' in resp['unmatched_name_ids'] 
+    assert 'Pan trodlogytes' in resp['unmatched_name_ids'] 
+
+def test_v2_tnrs_fuzzy():
     cfg = get_test_config()
     taxo = APIWrapper(config=cfg).taxomachine
     names = ['Pan trodlogytes', 'Homo sapphire', 'Plantago', 'Morpho peleides', 'Eleocharis',]
-    resp = taxo.TNRS(names, context_name="All life")
+    resp = taxo.TNRS(names, context_name="All life", fuzzy_matching=True)
+    print resp
     assert len(resp['matched_name_ids']) == 4
     assert resp['unmatched_name_ids'] == [u'Homo sapphire']
 
